062ea4ff5648ff68bfcb7a9a46f5249008be3975
ManifoldJaspar <82562742+manifoldj@users.noreply.github.com>
2021-04-15 06:28:13 +0100

    Create configure.sh

---
+++ configure/configure.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt-get update 
+sudo apt-get install libboost-all-dev libleveldb-dev  libcurl4-openssl-dev libmicrohttpd-dev libminiupnpc-dev libgmp-dev -y -qq
+sudo apt-get install zlib1g-dev libboost-tools-dev
